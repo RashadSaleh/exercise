d76d486b94c772d28d2675a596e5cb85d0a7a47f
Rashad Saleh <rashadsaleh@Rashads-MacBook-Pro.local>
2021-02-10 06:15:41 +0200

    this solution doesn't work yet, but it is a work in progress

--- js/main.js
+++ js/main.js
@@ -11,8 +11,52 @@ function getUserPassword() {
     return id('password').value
 }
 
-function guess() {
-    printGuess(getUserPassword());
+var charset = ("!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~").split("");
+var password = getUserPassword();
+
+var majorTries = 0;
+var length = 0;
+
+async function guessNextChar(initialGuess) {
+    for (i=0; i<charset.length; i++) {
+        nextChar = charset[i];
+        printGuess(initialGuess+nextChar);
+        await delay(20);
+        if (initialGuess+nextChar === password) {
+            return initialGuess+nextChar;
+        }
+    }
 }
 
-document.getElementById("try-me").onclick = guess;
+
+async function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
+ 
+async function guess(initialGuess) {
+
+    let x = initialGuess;
+
+    for (i=0; i<charset.length; i++) {
+        let found = await guessNextChar(x+charset[i]);
+        if (found) {
+            printGuess(found);
+            return;
+        }
+    }
+
+    if (majorTries === charset.length) {
+        majorTries = 0;
+        x += charset[length];
+        length++;
+    }
+    majorTries++;
+    x.replace(/.$/,charset[majorTries]);
+    guess(x);
+}
+
+document.getElementById("try-me").onclick = function() {
+    guess("")
+}
